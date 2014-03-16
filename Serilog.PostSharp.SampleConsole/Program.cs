f16e173a5633a548a1b3fe51c7be498a2d0cc8ee
Matthew Erbs <matthew.erbs@merbla.com>
2014-03-16 13:36:17 +1000

    Addded sample app

---
+++ Serilog.PostSharp.SampleConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.PostSharp.SampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Serilog.PostSharp/LogAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PostSharp.Aspects;
+
+namespace Serilog.PostSharp
+{
+    [Serializable]
+    public class LogAttribute : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            base.OnEntry(args);
+            Log.Debug("Method");
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            base.OnExit(args);
+            Log.Debug("Method");
+        }
+    }
+}
